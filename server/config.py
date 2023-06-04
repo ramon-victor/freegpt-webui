efeb37e9ae7be1121bdb4bd0515735dec0da3155
Ramon Victor Cardoso <ramonvc19@gmail.com>
2023-06-03 21:48:37 -0300

    Update models

--- server/config.py
+++ server/config.py
@@ -1,8 +1,6 @@
 models = {
     'text-gpt-0040-render-sha-0': 'gpt-4',
     'text-gpt-0035-render-sha-0': 'gpt-3.5-turbo',
-    'text-gpt-0035-render-sha-0301': 'gpt-3.5-turbo-0314',
-    'text-gpt-0040-render-sha-0314': 'gpt-4-0314',
 }
 
 special_instructions = {
