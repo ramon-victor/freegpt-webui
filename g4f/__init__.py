d4adf887be51420f09418a51c589947fabe666ff
Ramon Victor Cardoso <ramonvc19@gmail.com>
2023-06-27 13:47:19 -0300

    Adds all providers from the original API

--- g4f/__init__.py
+++ g4f/__init__.py
@@ -1,50 +1,36 @@
 import sys
-
-from .typing import MetaModels, Union
 from . import Provider
-
-
-class Model(metaclass=MetaModels):
-
-    class model:
-        name: str
-        base_provider: str
-        best_site: str
-
-    class gpt_35_turbo:
-        name: str = 'gpt-3.5-turbo'
-        base_provider: str = 'openai'
-        best_site: Provider.Provider = Provider.Forefront
-
-    class gpt_4:
-        name: str = 'gpt-4'
-        base_provider: str = 'openai'
-        best_site: Provider.Provider = Provider.Forefront
-
-
-class Utils:
-    convert: dict = {
-        'gpt-3.5-turbo': Model.gpt_35_turbo,
-        'gpt-4': Model.gpt_4
-    }
+from g4f.models import Model, ModelUtils
 
 
 class ChatCompletion:
     @staticmethod
-    def create(model: Model.model or str, messages: list, provider: Provider.Provider = None, stream: bool = False, **kwargs):
+    def create(model: Model.model or str, messages: list, provider: Provider.Provider = None, stream: bool = False, auth: str = False, **kwargs):
+        kwargs['auth'] = auth
+
+        if provider and provider.needs_auth and not auth:
+            print(
+                f'ValueError: {provider.__name__} requires authentication (use auth="cookie or token or jwt ..." param)', file=sys.stderr)
+            sys.exit(1)
+
         try:
             if isinstance(model, str):
-                model = Utils.convert[model]
+                try:
+                    model = ModelUtils.convert[model]
+                except KeyError:
+                    raise Exception(f'The model: {model} does not exist')
 
-            engine = model.best_site if not provider else provider
-            if not engine.supports_stream and stream:
+            engine = model.best_provider if not provider else provider
+
+            if not engine.supports_stream and stream == True:
                 print(
                     f"ValueError: {engine.__name__} does not support 'stream' argument", file=sys.stderr)
                 sys.exit(1)
 
+            print(f'Using {engine.__name__} provider')
+
             return (engine._create_completion(model.name, messages, stream, **kwargs)
                     if stream else ''.join(engine._create_completion(model.name, messages, stream, **kwargs)))
-
         except TypeError as e:
             print(e)
             arg: str = str(e).split("'")[1]
--- g4f/typing.py
+++ g4f/typing.py
@@ -1,15 +1,3 @@
 from typing import Dict, NewType, Union, Optional, List, get_type_hints
 
 sha256 = NewType('sha_256_hash', str)
-
-class MetaModels(type):
-    def __str__(self):
-        output: List = [
-            f'class Engines:\n',
-            f'  class {self.gpt_35_turbo.__name__}:',
-            '    ...',
-            f'  class {self.gpt_4.__name__}:',
-            '    ...',
-        ]
-
-        return '\n'.join(output)
