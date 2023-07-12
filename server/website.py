b635fd2025415db8fa6cc938c8008fc9ce7d2a6c
machsix <28209092+machsix@users.noreply.github.com>
2023-07-12 01:04:22 -0700

    use Blueprint to handle static files, add "url_prefix" in config.json for reverse proxy

--- server/website.py
+++ server/website.py
@@ -1,14 +1,15 @@
-from flask import render_template, send_file, redirect
+from flask import render_template, redirect, url_for
 from time import time
 from os import urandom
 
 
 class Website:
-    def __init__(self, app) -> None:
-        self.app = app
+    def __init__(self, bp, url_prefix) -> None:
+        self.bp = bp
+        self.url_prefix = url_prefix
         self.routes = {
             '/': {
-                'function': lambda: redirect('/chat'),
+                'function': lambda: redirect(url_for('._index')),
                 'methods': ['GET', 'POST']
             },
             '/chat/': {
@@ -18,24 +19,14 @@ class Website:
             '/chat/<conversation_id>': {
                 'function': self._chat,
                 'methods': ['GET', 'POST']
-            },
-            '/assets/<folder>/<file>': {
-                'function': self._assets,
-                'methods': ['GET', 'POST']
             }
         }
 
     def _chat(self, conversation_id):
         if '-' not in conversation_id:
-            return redirect('/chat')
+            return redirect(url_for('._index'))
 
         return render_template('index.html', chat_id=conversation_id)
 
     def _index(self):
-        return render_template('index.html', chat_id=f'{urandom(4).hex()}-{urandom(2).hex()}-{urandom(2).hex()}-{urandom(2).hex()}-{hex(int(time() * 1000))[2:]}')
-
-    def _assets(self, folder: str, file: str):
-        try:
-            return send_file(f"./../client/{folder}/{file}", as_attachment=False)
-        except:
-            return "File not found", 404
+        return render_template('index.html', chat_id=f'{urandom(4).hex()}-{urandom(2).hex()}-{urandom(2).hex()}-{urandom(2).hex()}-{hex(int(time() * 1000))[2:]}', url_prefix=self.url_prefix)
