921bba4e371f538942c73b1012ba881053b808f2
Ramon Victor Cardoso <ramonvc19@gmail.com>
2023-07-24 21:43:27 -0300

    Add auto compilation and language identification in translation folder

--- server/babel.py
+++ server/babel.py
@@ -1,24 +1,17 @@
+import os
+import subprocess
 from flask import request, session, jsonify
 from flask_babel import Babel
 
-BABEL_DEFAULT_LOCALE = 'en_US'
-BABEL_LANGUAGES = [
-    'en_US',  
-    'es_MX',  
-    'pt_BR',  
-    'ru_RU',  
-    'zh_CN',  
-    'zh_TW',  
-    'fr_FR', 
-    'de_DE',  
-    'it_IT', 
-    'ja_JP',  
-    'ko_KR', 
-    'ar_SA', 
-    'hi_IN',  
-    'tr_TR',  
-]
 
+def get_languages_from_dir(directory):
+    """Return a list of directory names in the given directory."""
+    return [name for name in os.listdir(directory)
+            if os.path.isdir(os.path.join(directory, name))]
+
+
+BABEL_DEFAULT_LOCALE = 'en_US'
+BABEL_LANGUAGES = get_languages_from_dir('translations')
 
 
 def create_babel(app):
@@ -28,16 +21,28 @@ def create_babel(app):
     app.config['BABEL_LANGUAGES'] = BABEL_LANGUAGES
 
     babel.init_app(app, locale_selector=get_locale)
+    compile_translations()
 
 
 def get_locale():
     """Get the user's locale from the session or the request's accepted languages."""
-    user_language = session.get('language')
-    if user_language is not None:
-        return user_language
-
-    return request.accept_languages.best_match(BABEL_LANGUAGES)
+    return session.get('language') or request.accept_languages.best_match(BABEL_LANGUAGES)
 
 
 def get_languages():
+    """Return a list of available languages in JSON format."""
     return jsonify(BABEL_LANGUAGES)
+
+
+def compile_translations():
+    """Compile the translation files."""
+    result = subprocess.run(
+        ['pybabel', 'compile', '-d', 'translations'],
+        stdout=subprocess.PIPE,
+    )
+
+    if result.returncode != 0:
+        raise Exception(
+            f'Compiling translations failed:\n{result.stdout.decode()}')
+
+    print('Translations compiled successfully')
